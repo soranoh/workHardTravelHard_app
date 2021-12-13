83a563bdc7b3ea656f3ce9b2eab09d3463216073
soranoh <sora2019@naver.com>
2021-12-13 13:38:21 +0900

    #3.2 TextInput

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { theme } from "./colors";
 
 /*
- * 1) TouchableOpacity : 이벤트를 listen 할 준비가 되어있는 View
- *    touchable + opacity => 클릭하면 잠시 투명해진다.
- * 2) TouchableHighlight : 요소를 클릭할 경우 배경색이 바뀌게 해준다.
- *    underlayColor 로 설정할 수 있다.
- * 3) TouchableWithoutFeedback : 화면의 가장 위에서 일어나는 탭 이벤트를 listen 하는 Touchable 컴포넌트이다.
- *    그래픽이나 다른 UI 반응을 보여주지는 않는다.
- * 4) Pressable : 이번에 새로나온거
+ * 1) 
  */
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true); 
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
         </TouchableOpacity>
-        <TouchableWithoutFeedback>
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableWithoutFeedback>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
+        </TouchableOpacity>
       </View>
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -41,8 +47,15 @@ const styles = StyleSheet.create({
     marginTop: 100,
   },
   btnText: {
-    color: "white",
     fontSize: 38,
     fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18
   }
 });
--- colors.js
+++ colors.js
@@ -1,4 +1,4 @@
 export const theme = {
     bg: "black",
-    gray: "#3A3D40"
+    grey: "#3A3D40"
 }
